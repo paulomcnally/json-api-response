5e9d106b7658747a8ab6a55bf0f510cb9161fb12
Paulo McNally <paulomcnally@gmail.com>
2014-02-22 17:46:55 -0600

    1.0.0

--- test/test.js
+++ test/test.js
@@ -1,16 +1,35 @@
 var jar = require('../lib/json-api-response');
 
 
-var error = jar.error(1,'Example','Error generado por el sistema');
+var error = jar.error(1,'ExampleException','This is a error');
 
+console.log('Error example');
 console.log( error );
 
 
-var response = jar.response( {"text": "demo"} );
+var response = jar.response( {"foo": "bar"} );
 
+console.log('Response example');
 console.log( response );
 
 
-var responseFail = jar.response( "Respuesta" );
+var responseFail = jar.response( "response" );
 
+console.log('Fail example');
 console.log( responseFail );
+
+// express
+var express = require('express');
+var app = express();
+
+app.use( jar.express );
+
+app.get('/error',function(req,res){
+   res.jarError(1,'ExampleException','This is a error');
+});
+
+app.get('/response',function(req,res){
+   res.jarResponse({"foo":"bar"});
+});
+
+app.listen(8080);
